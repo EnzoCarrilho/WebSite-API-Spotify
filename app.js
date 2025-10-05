5ae1bfc4dcc304b28142f410bf60ac10c7a3b822
EnzoCarrilho <enzocarrilho25@gmail.com>
2025-10-05 13:15:43 -0300

    funções para busca de um album por nome do artista

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const APIController = (function () {
     
     const getToken = async () => {
 
-        const result = await fetch('http://accounts.spotify.com/api/token', {
+        const result = await fetch('https://accounts.spotify.com/api/token', {
             method: 'POST',
             headers: {
                 'CONTENT-TYPE' : 'application/x-www-form-urlencoded',
@@ -20,10 +20,50 @@ const APIController = (function () {
 
         const data = await result.json()
         return data.access_token
-        
+    }
+
+    const getArtistIdByName = async (artistName, token) => {
+    const response = await fetch(`https://api.spotify.com/v1/search?q=${encodeURIComponent(artistName)}&type=artist`, {
+        headers: {
+            'Authorization': 'Bearer ' + token
+        }
+    })
+
+        const data = await response.json();
+        const artist = data.artists.items[0]; // pega o primeiro resultado
+        return artist ? artist.id : null;
+    }
+
+    const getArtistAlbums = async (artistId, token) => {
+    const result = await fetch(`https://api.spotify.com/v1/artists/${artistId}/albums?include_groups=album,single&limit=50`, {
+        headers: {
+            'Authorization': 'Bearer ' + token
+        }
+    });
+
+        const data = await result.json();
+        return data.items;
     }
 
     
+    return{
+        getToken(){
+            return getToken()
+        },
+        getArtistIdByName(artistName, token){
+            return getArtistIdByName(artistName, token)
+        },
+        getArtistAlbums(artistId, token){
+            return getArtistAlbums(artistId, token)
+        }
+    }
 
-    
-})
+})();
+
+//const apiController = APIController
+//const token = await apiController.getToken()
+//console.log('Token atual: ' + token)
+//const idArista = await apiController.getArtistIdByName('Led Zeppelin', token)
+//console.log('ID do artista: ' + idArista)
+//const albumsDoArtista = await apiController.getArtistAlbums(idArista, token)
+//console.log(albumsDoArtista)
